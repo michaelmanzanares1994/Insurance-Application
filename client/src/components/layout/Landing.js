9865518f1f58d2fedd8b2643c0b90ab03dd73583
michael m <mmanz1994@gmail.com>
2018-10-23 16:04:05 -0500

    implemented routing and front end registration logic

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Insurance Website</h1>
+                <p className="lead">
+                  {" "}
+                  Create a quote or policy quickly with this secure full stact
+                  insurance website
+                </p>
+                <hr />
+                <br /> <br /> <br /> <br />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
